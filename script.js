c4d44e06b3e68428efa8779f8d2f4920363a624a
EbuElectrify <EbuElectrify@gmail.com>
2022-06-24 02:35:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Name: Onuh Prince Ebubechukwu");
+alert("Height: 5'40ft");
+alert("Country: Nigeria");
+
+//Can also be viewed on the conmsole
+console.log(
+    'Name: Onuh Prince Ebubechukwu',
+    "Height: 5'40ft",
+    "Country: Nigeria"
+);
